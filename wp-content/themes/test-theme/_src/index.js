0bf9e647b80d359cd0cae11f395e10728b5a9d93
Hanna Domagała <hdomagala@goldencomm.com>
2020-07-15 23:28:09 +0200

    Set up FED theme basics

---
+++ wp-content/themes/test-theme/_src/index.js
@@ -0,0 +1,5 @@
+// STYLES
+import './styles/main.scss';
+
+// SCRIPTS
+import './scripts/main.js';
---
+++ wp-content/themes/test-theme/webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const glob = require('glob');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// STYLES
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+
+// IMAGES
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+const PATHS = {
+    src: path.join(__dirname)
+};
+
+module.exports = (env, argv) => {
+    const settings = {
+        mode: 'development',
+        entry: './_src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'assets'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s?css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                url: false,
+                                importLoaders: 1,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                    ]
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: "main.css",
+            })
+        ]
+    };
+
+    if (argv.mode === 'development') {
+        settings.devtool = "inline-source-map"
+    }
+
+    if (argv.mode === 'production') {
+        settings.plugins.push(
+            new PurgeCSSPlugin({
+                paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+            })
+        );
+        settings.plugins.push(
+            new ImageminPlugin({
+                test: /\.(gif|jpe?g|png|svg)$/,
+                gifsicle: {
+                    interlaced: true,
+                    optimizationLevel: 3,
+                },
+                jpegtran: {progressive: true},
+                optipng: {optimizationLevel: 7},
+            })
+        );
+    }
+
+    return settings;
+};
